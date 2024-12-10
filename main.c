e75f33165a61a45662d0dee956c0c40873532c8e
Sergei Aidinov <incoming34@yandex.ru>
2024-12-10 18:33:24 +0300

    Cleaned code

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum cell { X, O, EMPTY };
+enum cell { CROSS, NOUGHT, EMPTY };
 
 enum cell playing_field[3][3];
 const int UPPER_LEFT_CORNER = 0x2554;
@@ -16,6 +16,9 @@ const int REVERSED_T_SHAPED_CROSS = 0x2569;
 const int GAP = 32;
 const int LINE_LENGTH = 12;
 const int MULTIPLICITY = 4;
+const char CROSS_SIGN = 'X';
+const char NOUGHT_SIGN = 'O';
+const char EMPTY_SIGN = ' ';
 
 void draw_upper_line();
 
@@ -26,6 +29,8 @@ void draw_row(int row);
 int main(void) {
     setlocale(LC_ALL, "");
     draw_upper_line();
+    playing_field[0][1] = EMPTY;
+    playing_field[0][2] = NOUGHT;
     draw_row(0);
     draw_bottom_line();
     return EXIT_SUCCESS;
@@ -55,21 +60,18 @@ void draw_bottom_line() {
 
 void draw_row(int row) {
     printf("%lc", VERTICAL_BORDER);
+    int cell_order = 0;
     for (int i = 1; i < LINE_LENGTH; i++) {
         if (i >= MULTIPLICITY & (i % MULTIPLICITY) == 0) {
             printf("%lc", VERTICAL_BORDER);
         } else if (i >= 2 & (i % 2) == 0) {
-            if (playing_field[i][row] == EMPTY) printf("%c", ' ');
-            else if (playing_field[i][row] == X) printf("%c", 'X');
-            else if (playing_field[i][row] == O) printf("%c", 'O');
+            if (playing_field[row][cell_order] == EMPTY) printf("%c", EMPTY_SIGN);
+            else if (playing_field[row][cell_order] == CROSS) printf("%c", CROSS_SIGN);
+            else if (playing_field[row][cell_order] == NOUGHT) printf("%c", NOUGHT_SIGN);
+            cell_order++;
         } else {
             printf("%lc", GAP);
         }
-
-
-        /*if (playing_field[i][row] == EMPTY) printf("%c", ' ');
-        else if (playing_field[i][row] == X) printf("%c", 'X');
-        else if (playing_field[i][row] == O) printf("%c", 'O');*/
     }
     printf("%lc", VERTICAL_BORDER);
     printf("\n");

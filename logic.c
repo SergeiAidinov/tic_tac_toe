9e37e51bf8a56271f4ffac81ba18daca8d4641ff
Sergei Aidinov <incoming34@yandex.ru>
2024-12-24 22:10:20 +0300

    try to allocate memory

--- logic.c
+++ logic.c
@@ -1,6 +1,8 @@
 #include "utils.h"
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 extern enum TOKEN get_token(int column, int row);
@@ -58,9 +60,11 @@ enum WINNER check_winner() {
 
 struct input user_input() {
   char letter;
-  //char figure;
   char * inp;
-  do{
+  do {
+      int input_is_valid = 1;
+      int message_lenght = 0;
+      char *error_message = NULL;
       char *str = NULL, c;
       int len = 1;
       str = (char*) malloc(sizeof(char));
@@ -71,20 +75,48 @@ struct input user_input() {
           str = (char*) realloc(str, len);
       }
       if ( (len - 1) > MAX_USER_INPUT_LENGTH || len < MIN_USER_INPUT_LENGTH) {
-        printf("input too big or too small\n");
+        error_message = "Input is too long or too short. ";
+        /*
+        message_lenght = strlen(error_message) + 1;
+        error_message = (char*) malloc(sizeof(char) * message_lenght);
+*/
+        //error_message = mes;
+        //printf("%s", error_message);
+        input_is_valid = 0;
       }
-      if (str[0] < 65 || str[0] > 90) {
-        printf("First symbol has to be a letter");
+      str[0] = toupper(str[0]);
+      //Проверяем букву
+      if ((str[0] < 65 || str[0] > 90)) {
+        printf("First symbol has to be a letter. ");
+        input_is_valid = 0;
       }
+      // Проверяем цифры
+      int is_number = 1;
       for (int i = 1; i < (len - 1); i++) {
         char figure = str[i];
         if (figure < 48 || figure > 57) {
-            printf("Second and following symbols have to be figures");
+            is_number = 0;
+            input_is_valid = 0;
         }
       }
+      if (!is_number) {
+        char *qqq = "Second and following symbols have to be figures. ";
+        // str = (char*) realloc(str, len);
+        message_lenght +=  snprintf(NULL, 0, "%s", qqq) + sizeof('\0');
+        char *new_error_message = (char*) malloc(message_lenght);
+        new_error_message = strcpy(error_message, qqq);
+
+        printf("%s", error_message);
+        /*
+        char * existing_message = error_message;
+        error_message = (char*) realloc(existing_message, (sizeof(char) * (strlen(existing_message) + (strlen(mes1) + 1))));
+        error_message = strcat(error_message, mes1);
+        printf("%s", error_message);
+*/
+      }
       str[len - 1] = '\0';
-      printf("%s (%d symbols)\n", str, len - 1);
+      //printf("%s (%d symbols)\n", str, len - 1);
       free(str);
-  }while(1);
+  } while(1);
 
 }
